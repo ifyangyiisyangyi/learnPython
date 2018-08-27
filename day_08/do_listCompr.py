dcf8f72fa7c6f5878a559a4ca79532f3dd231a48
yangyi <yangyi7351598@gmail.com>
2018-08-27 16:40:38 +0800

    update day_08

--- day_08/do_listCompr.py
+++ day_08/do_listCompr.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 L1 = ['Hello', 'World', 18, 'Apple', None]
-
-# L2 = [s.lower() for s in L1 if isinstance (s, str)]
-L2 = []
-for s in L1:
-	if isinstance (s, str):
-		s = s.lower()
-		L2.append(s)
-
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
 # 测试:
 print(L2)
 if L2 == ['hello', 'world', 'apple']:
