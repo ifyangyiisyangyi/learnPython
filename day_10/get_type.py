267eb718e6a86ed29aa97a1c7f11f8a32fc44380
yangyi <yangyi7351598@gmail.com>
2018-09-10 17:54:37 +0800

    获取数据类型

---
+++ day_10/get_type.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# python内置方法 type() 可以判断数据类型
+print('type(123) = ', type(123))
+print('type(\'123\') = ', type('123'))
+print('type(None) = ', type(None))
+print('type(abs) = ', type(abs))
+
+# import types
+
+print('type(\'abc\') == str?', type('abc') == str)
