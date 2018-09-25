d51f93ee56d228591f6ed003fdc5b9a162f0f446
yangyi <yangyi7351598@gmail.com>
2018-09-25 16:21:42 +0800

    how to io file

---
+++ day_14/file_io.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# 打开一个文件对象，并读取
+# f = open('c:/aaa/aaa.txt', 'r')
+# print(f.read())
+# f.close()
+
+# try...finally
+# try:
+# 	f = open('c:/aaa/aaa.txt','r')
+# 	print(f.read())
+# finally:
+# 	if f:
+# 		f.close()
+
+# with 方法读文件
+# with open('c:/aaa/aaa.txt', 'r') as f:
+# 	print(f.read())
+
+# with方法写文件
+# with open('c:/aaa/aaa.txt', 'w') as f:
+	# f.write('hello, world')
