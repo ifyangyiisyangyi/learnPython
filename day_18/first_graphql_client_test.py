494a4aecc7af51aa8a6b8e3e72378492035ca9d8
yangyi <yangyi7351598@gmail.com>
2019-05-06 16:26:07 +0800

    add graphql test

--- day_18/first_graphql_client_test.py
+++ day_18/first_graphql_client_test.py
@@ -1,22 +1,30 @@
-
+'''
+python请求graphql登录接口，返回token
+'''
+import json
 from graphqlclient import GraphQLClient
 
-client = GraphQLClient('http://luka-graphql.zhangfeifei.linglove.cn/graphql')
-query = """
-mutation signIn($region: Region!, $name: String!, $password: String!){
-  signIn(region: $region, name: $name, password: $password){
-    token
-    tokenType
+
+def getToken():
+  client = GraphQLClient(
+      'http://luka-graphql.test1.k8s-qa.linglove.cn/graphql')
+  query = """
+  mutation signIn($region: Region!, $name: String!, $password: String!){
+    signIn(region: $region, name: $name, password: $password){
+      token
+      tokenType
+    }
   }
-}
-"""
-variables = {
-    "region": "CN",
-    "name": "18510086742",
-    "password": "12345678"
-}
+  """
+  variables = {
+      "region": "CN",
+      "name": "18859285396",
+      "password": "123456a"
+  }
+  result = client.execute(query=query, variables=variables)
+  token = json.loads(result)['data']['signIn']['token']
+  return token
 
 
-result = client.execute(query=query, variables=variables)
-
-print(result)
+# 测试
+print(getToken())
