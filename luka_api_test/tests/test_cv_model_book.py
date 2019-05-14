494465bac78a45866ff75a2ec4cb2b3daf43887a
杨毅 <yangyi@ling.ai>
2019-05-14 21:40:59 +0800

    luka_api测试

---
+++ luka_api_test/tests/test_cv_model_book.py
@@ -0,0 +1,42 @@
+# '''
+# 绘本识别测试
+# '''
+# import test_robot_login
+# import pytest
+# import requests
+# import json
+# import read_yaml
+# import get_robot_token
+
+
+# udid = "FRFFBZYP"
+# book_id = "50"
+# token = get_robot_token.get_robot_token(udid)
+
+# def cv_model_book(book_id):
+#     # print(token)
+#     urll = read_yaml.test_data["url"] + "/cv/model/book/" + book_id
+#     # print(urll)
+#     headers = {
+#         "Authorization":"Bearer" + token,
+#         "Accept":"application/vnd.luka.v1.14+json"
+#     }
+#     data = {
+
+#     }
+#     result = requests.get(url = urll,headers = headers, data = json.dumps(data))
+#     result = json.loads(result.text)
+#     result = result["errmsg"]
+#     return result
+
+# class TestCase():
+#     @pytest.mark.smoke
+#     def test_book_read_case1(self):
+#         print("绘本id 50测试")
+#         assert cv_model_book("50") == "success"
+#     @pytest.mark.skip(reason = "skip跳过")     # 标记为跳过，不执行该用例
+#     def test_book_read_case2(self):
+#         assert cv_model_book("415") == "success"
+#     @pytest.mark.skipif(json.__version__ > "100", reason = "skipif跳过这条用例")
+#     def test_book_read_case3(self):
+#         assert cv_model_book("12047") == "success"
---
+++ luka_api_test/tests/test_robot_login.py
@@ -0,0 +1,13 @@
+'''
+登录接口测试
+'''
+
+import sys
+sys.path.append('..')
+import pytest
+import requests
+import json
+import interface.read_yaml
+
+
+print(interface.read_yaml.test_data)
