82ab6c945ffcff679a904c01fd8fec25786bc87d
杨毅 <yangyi@ling.ai>
2019-05-24 22:56:24 +0800

    环境变量参数化

--- luka_api_test/conf/read_env.py
+++ luka_api_test/conf/read_env.py
@@ -3,13 +3,20 @@
 '''
 import yaml
 import os
+import json
 
 filePath = os.path.dirname(os.path.realpath(__file__))  # 返回.py文件的绝对路径
 yamlPath = os.path.join(filePath, "env.yml")  # 路径拼接，返回配置文件的绝对路径
-f = open(yamlPath, 'r', encoding='utf-8')
-result = f.read()
-test_data = yaml.load(result, Loader=yaml.FullLoader) # PyYaml 5.1版本弃用了load()原本用法，需要加上 Loader
-
+envPath = os.path.join(filePath, 'current_env.json')
+with open(yamlPath, 'r', encoding='utf-8') as f:
+    result = f.read()
+    test_data = yaml.load(result, Loader=yaml.FullLoader) # PyYaml 5.1版本弃用了load()原本用法，需要加上 Loader
+if os.path.exists(envPath):
+    with open(envPath, 'r', encoding='utf-8') as f:
+        result = f.read()
+        result = json.loads(result)
+    for key in result:
+        test_data[key] = result[key]
 
 if __name__ == '__main__':
     print(test_data)
--- luka_api_test/run.py
+++ luka_api_test/run.py
@@ -1,21 +1,34 @@
 import pytest
 import os
 import time
+import json
 
 
-def main(device_type='luka'):
+def main(device_type='luka', **kwargs):
+    current_env_dict = {}
+    for key in kwargs:
+        current_env_dict[key] = kwargs[key]
+
+    env_path = os.path.join(os.path.abspath('.'), 'conf/current_env.json')
+    with open(env_path, 'w') as f:
+        json.dump(current_env_dict, f)
+
     print(os.path.abspath('.'))
-    path = os.path.join(os.path.abspath('.'), 'report/')
-    print(path)
+    report_path = os.path.join(os.path.abspath('.'), 'report/')
+    print(report_path)
     now = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
 
     if device_type == 'luka':
-        luka_report_name = path + 'luka-' + now + '.html'
+        luka_report_name = report_path + 'luka-' + now + '.html'
         pytest.main(['-v', '--html=' + luka_report_name, '--self-contained-html'])
     elif device_type == 'baby':
-        baby_report_name = path + 'baby-' + now + '.html'
+        baby_report_name = report_path + 'baby-' + now + '.html'
         pytest.main(['-m', 'baby', '-v', '--html=' + baby_report_name, '--self-contained-html'])
 
+    if (os.path.exists(env_path)):
+        os.remove(env_path)
+    else:
+        print('current_env is not exists~')
 
 if __name__ == '__main__':
     main()
