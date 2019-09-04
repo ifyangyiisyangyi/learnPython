156b0a89e02725d6d0d209fea730a084d62cfa40
杨毅 <yangyi@ling.ai>
2019-09-04 21:11:16 +0800

    控制代码块&登录表单

--- learnFlask/demo/hello.py
+++ learnFlask/demo/hello.py
@@ -68,5 +68,33 @@ def get_order(order_id):
     return f'order id is {order_id}'
 
 
+'''
+目的：实现一个简单的登录逻辑处理
+1. 路由实现get,post两种请求方式 --> 需要判断请求参数
+2. 获取请求的参数
+3. 判断参数是否填写
+4. 如果判断没问题，就返回success
+'''
+
+
+# 登录表单
+@app.route('/index/', methods=['GET', 'POST'])
+def index():
+    # 1. 判断请求方式
+    if request.method == 'POST':
+        # 2. 获取请求参数
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(type(username))
+        print(f'password:{password}')
+        # 3. 判断参数是否填写
+        if username == '' or password == '':
+            print('参数不完整')
+        else:
+            return 'success'
+
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
