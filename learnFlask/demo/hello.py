984bcb5c1744c6ea5c3191bef18aa2f128a4df83
杨毅 <yangyi@ling.ai>
2019-09-04 20:03:19 +0800

    变量代码块的使用

--- learnFlask/demo/hello.py
+++ learnFlask/demo/hello.py
@@ -3,7 +3,7 @@ flask test
 
 """
 
-from flask import Flask, url_for
+from flask import Flask, request
 from flask import render_template
 import requests
 import json
@@ -13,6 +13,7 @@ app = Flask(__name__)
 url = "http://luka-api.test1.k8s-qa.linglove.cn"
 
 
+# 登录
 @app.route('/login/')
 def get_app_token():
     urll = url + "/app-login"
@@ -33,18 +34,38 @@ def get_app_token():
     }
     result = requests.put(urll, headers=headers, data=json.dumps(data))
     result = json.loads(result.text)
-    token = result["data"]["token"]
-    return token
+    res = json.dumps(result)
+    return res
 
 
+# html模板
 @app.route('/html/')
 def test():
-    return render_template('hello.html')
+    url_str = 'www.yy.com'
+    my_list = [1, 2, 3]
+    my_dict = {'name': 'yy', 'age': 30}
+    return render_template('hello.html', url_str=url_str, my_list=my_list, my_dict=my_dict)
 
 
+# js模板
 @app.route('/')
 def js():
     return render_template('index.html')
+
+
+# 请求对象
+@app.route('/method/', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        return 'success'
+    else:
+        return 'fail'
+
+
+# 请求传参
+@app.route('/orders/<int:order_id>')
+def get_order(order_id):
+    return f'order id is {order_id}'
 
 
 if __name__ == '__main__':
