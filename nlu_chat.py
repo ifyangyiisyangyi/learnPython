4d2fca99db177c0ae0799f404872a1823b44a06d
杨毅 <yangyi@ling.ai>
2019-09-26 19:15:43 +0800

    add nlu device_type

--- nlu_chat.py
+++ nlu_chat.py
@@ -8,15 +8,15 @@ import pywin32_system32
 import time
 import yaml
 
+url_online = "https://luka-baby-prerelease.ling.cn/robot-login"
+url_nlu_chat = "https://luka-baby-prerelease.ling.cn/nlu/chat"
 
-url_online = "https://luka-api.ling.cn/robot-login"
-url_nlu_chat = "https://luka-api.ling.cn/nlu/chat"
-
-udid = "FRFFBZYP"
-api_version = "v1.14"
+udid = "5C7JYEY2"
+api_version = "v1.1"
 Language = "zh_CN"
-accept = "application/vnd.luka." + str(api_version) + "+json"
+accept = "application/vnd.luka-baby." + str(api_version) + "+json"
 inputfile = "nlutest.txt"
+
 
 # 获取token
 def get_token(url):
@@ -38,6 +38,7 @@ def get_token(url):
     token = result["data"]["token"]
     return token
 
+
 # nlu/chat
 def nlu_chat(token, url):
     Authorization = "Bearer" + token
@@ -53,30 +54,31 @@ def nlu_chat(token, url):
             data = {
                 "data": {
                     "type": "nlu", "attributes": {
-                        "user_id": "luka_02", "words": words, "machine_sentence": "", "initiative": bool(0), "use_chitchat": 1
+                        "user_id": "luka_02", "device_type": "lukababy", "words": words, "machine_sentence": "",
+                        "initiative": bool(0),
+                        "use_chitchat": 1
                     }
                 }
             }
             print("————————————————————————————————————————————")
             print(words)
-            engine = pyttsx3.init()
-            engine.say("卢卡卢卡")
-            engine.say(words)
-            engine.runAndWait()
+            #         engine = pyttsx3.init()
+            #         engine.say("卢卡卢卡")
+            #         engine.say(words)
+            #         engine.runAndWait()
 
-            
             result = requests.post(url=url, headers=headers, data=json.dumps(data))
-            result = json.loads(result.text)   
-            script = base64.b64decode(result["data"]["script"]) 
+            result = json.loads(result.text)
+            script = base64.b64decode(result["data"]["script"])
             yamlScript = yaml.load_all(script)
             for i in yamlScript:
                 result = i.get("main")
-                print(result)
+                # print(result)
                 for j in result:
-                    result = j.split(" ")[-1]  
-                    print(str(base64.b64decode(result),"utf-8"))
-            time.sleep(5)
+                    result = j.split(" ")[-1]
+                    print(str(base64.b64decode(result), "utf-8"))
+            # time.sleep(5)
+
 
 if __name__ == "__main__":
     nlu_chat(get_token(url_online), url_nlu_chat)
-    
