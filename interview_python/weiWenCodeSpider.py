58bb21ee08f3843bfed65aa54ef3429fde0301ae
ifyangyiisyangyi <yangyi7351598@gmail.com>
2019-11-08 17:50:22 +0800

    爬取未闻code文章标题和链接

--- interview_python/weiWenCodeSpider.py
+++ interview_python/weiWenCodeSpider.py
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
+"""
+爬取未闻code的文章和标题
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_article_page(url):
+    r = requests.request('get', url=url)
+    html = r.text
+    soup = BeautifulSoup(html, 'lxml')
+    ls = soup('h3', class_="com-article-panel-title")
+    url_dict = {}
+    for tag in ls:
+        s = 'https://cloud.tencent.com' + tag.a['href']
+        title = tag.a.string
+        url_dict[title] = s  # 返回文章的标题和链接
+    return url_dict
+
+
+def get_article_all():
+    url_dict = {}
+    for i in range(30):
+        url = 'https://cloud.tencent.com/developer/column/5263/page-' + str(i)
+        url_sigle_dict = get_article_page(url)
+        url_dict = dict(url_dict, **url_sigle_dict)
+    return url_dict
+
+
+if __name__ == '__main__':
+    s = get_article_all()
+    print(s)
+    print(len(s))
+=======
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -32,3 +68,4 @@ for i in htmlList:
 	
 # test
 
+>>>>>>> b8af538b548cb18d777bb25838d12c51ffe2f5c1
