779cac34c07d9055647eff4d3d98a17892003d9b
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-04-16 21:56:07 +0800

    使用模板

--- learnDjango/djangoDemo/view.py
+++ learnDjango/djangoDemo/view.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
- return HttpResponse("Hello world ! ")
+    context         = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
