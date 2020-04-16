4301b8accc1bdc32345656cbc9c4d17b7adb6059
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-04-16 22:01:55 +0800

    {% include %}标签允许在模板中包含其他模板的内容

--- learnDjango/djangoDemo/view.py
+++ learnDjango/djangoDemo/view.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 def hello(request):
     context         = {}
     context['hello'] = 'Hello World!'
+    context['inc'] = 'include test~'
     return render(request, 'hello.html', context)
