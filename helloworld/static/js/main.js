72ffd42e3d5a025092d95b7a413adc0116ce05ef
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-06-07 11:51:41 +0800

    优化模板

--- helloworld/static/js/main.js
+++ helloworld/static/js/main.js
@@ -3,7 +3,7 @@
   $(window).on("load", function() { // makes sure the whole site is loaded
     //preloader
     $("#status").fadeOut(); // will first fade out the loading animation
-    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+    $("#preloader").delay(1000).fadeOut("slow"); // will fade out the white DIV that covers the website.
     
     //masonry
     $('.grid').masonry({
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["我叫杨毅", "我是一名测试工程师", "我和BUG势不两立"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
