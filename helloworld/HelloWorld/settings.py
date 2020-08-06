e95f71d814de6fd6db6bd286dcb467ecff4658b7
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-08-06 23:58:20 +0800

    测试阿里云发邮件

--- helloworld/HelloWorld/settings.py
+++ helloworld/HelloWorld/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',  # 消息框架
     'django.contrib.staticfiles',  # 管理静态文件框架
     'TestModel',
-    'django-crontab',  # 定时任务
+    'django_crontab',  # 定时任务
 ]
 
 CRONJOBS = [
