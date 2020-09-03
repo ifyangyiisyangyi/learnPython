ab57a4d74eb6694b69b5b2dbb20c5a58fb25d277
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-09-04 00:44:42 +0800

    add article model && add Markdown > requirements.txt

--- helloworld/article/urls.py
+++ helloworld/article/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'article'
 urlpatterns = [
+    # path('hello', IndexView.as_view(), name='index_2'),  # 主页，自然排序
     path('test/', views.test, name='test'),
 ]
--- helloworld/article/views.py
+++ helloworld/article/views.py
@@ -1,5 +1,11 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 
 # Create your views here.
+from article.models import Article
+
+
 def test(request):
-    return render(request, 'detail.html')
+    body = model_to_dict(Article.objects.get(id=1))
+    print(body)
+    return render(request, 'detail.html', {'body' : body})
