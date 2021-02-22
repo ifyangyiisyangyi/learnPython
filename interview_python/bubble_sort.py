17bae1405c73887568d45b4444bfdefee16ea8b8
ifyangyiisyangyi <yangyi7351598@gmail.com>
2021-02-22 17:52:34 +0800

    numpy生成随机列表

--- interview_python/bubble_sort.py
+++ interview_python/bubble_sort.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 # 冒泡排序
+
+import numpy as np
+
+
 def bubble_sort(list):
-	for i in list:
-		for j in range(len(list) - 1):
-			if list[j] > list[j + 1]:
-				list[j], list[j+1] = list[j+1], list[j]
-arr = [1,5,3,13131,12431432,121312412,2]
+    for i in list:
+        for j in range(len(list) - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+
+
+arr = np.random.randint(1000, size=10)  # 生成1000以内的10个随机值
 bubble_sort(arr)
 print(arr)
