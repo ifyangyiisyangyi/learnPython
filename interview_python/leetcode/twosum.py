b67861d27a7aa30117edf55edb0660f87d155453
ifyangyiisyangyi <yangyi7351598@gmail.com>
2022-12-10 10:02:09 +0800

    leecode 1 两数之和

---
+++ interview_python/leetcode/twosum.py
@@ -0,0 +1,22 @@
+# 1. 两数之和
+
+'''
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+'''
+from typing import List
+
+
+class Solution:
+    def twosum(self, nums: List[int], target: int) -> List[int]:
+        l = len(nums)
+        for i in range(l):
+            for j in range(i + 1, l):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__ == '__main__':
+    a = Solution()
+    assert a.twosum([2, 7, 11, 15], 9) == [0, 1]
+    assert a.twosum([3, 2, 4], 6) == [1, 2]
+    assert a.twosum([3, 3], 6) == [0, 1]
